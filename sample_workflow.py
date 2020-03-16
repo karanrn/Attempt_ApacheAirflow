22108383a0d8a041654644f4dc80b4b480eff621
Karan <karan4080@gmail.com>
2020-03-16 13:28:20 +0530

    Added MySQL operation on compeletion of tasks.

--- sample_workflow.py
+++ sample_workflow.py
@@ -3,9 +3,11 @@ from datetime import datetime
 from airflow import DAG
 from airflow.operators.bash_operator import BashOperator
 from airflow.operators.python_operator import PythonOperator
+from airflow.operators.mysql_operator import MySqlOperator
 from airflow.utils.dates import days_ago
 from datetime import datetime
 import os
+import random
 # from write_date_to_file import WriteToFile
 
 default_args = {
@@ -18,7 +20,7 @@ default_args = {
 }
 
 # Path for output files
-path = '/home/karan/Airflow/'
+path = '/home/karan/Attempt_ApacheAirflow/'
 
 dag = DAG(
     'sample_workflow',
@@ -30,7 +32,7 @@ dag = DAG(
 
 # task 1
 command1 = """
-echo "Time: $(date)" >> /home/karan/Airflow/t1.log
+echo "Time: $(date)" >> /home/karan/Attempt_ApacheAirflow/t1.log
 """
 t1 = BashOperator(
     task_id='print_date',
@@ -44,7 +46,7 @@ t1 = BashOperator(
 
 # task 2
 command2 = """
-touch /home/karan/Airflow/t2-$(date +"%T").log
+touch /home/karan/Attempt_ApacheAirflow/t2-$(date +"%T").log
 """
 t2 = BashOperator(
     task_id='create_file',
@@ -52,10 +54,11 @@ t2 = BashOperator(
     bash_command=command2,
     dag=dag
 )
+t2_complete = datetime.now()
 
 # task 3 - python function
 def WriteToFile():
-    dir_path = '/home/karan/Airflow/'
+    dir_path = '/home/karan/Attempt_ApacheAirflow/'
     file_name = 't3.log'
     full_path = dir_path + file_name
     f = open(full_path, "a+")
@@ -70,5 +73,19 @@ t3 = PythonOperator(
     email_on_failure=True,
     dag=dag
 )
+
+t3_complete = datetime.now()
+t3_status_sql = """
+insert into stage_status values (3, '{}', 'python_create_file', 'success'),
+(2, '{}', 'create_file', 'success');
+""".format(str(t3_complete), str(t2_complete))
+
+t4 = MySqlOperator(
+    task_id='Update_status_table',
+    sql=t3_status_sql,
+    mysql_conn_id='local_mysql',
+    owner='airflow',
+    dag=dag
+)
 # Dependancy
-t1 >> [t2, t3]
+t1 >> [t2, t3] >> t4
